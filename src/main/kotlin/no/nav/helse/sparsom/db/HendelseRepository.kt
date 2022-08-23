18d10dee454570a0e95bc92ba1e3835f7ab28229
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2022-08-23 08:42:00 +0200

    Revert "Initial commit"
    This reverts commit 37daabbb3adea3b720dde4b5c33c3d69c49e69a1.

--- src/main/kotlin/no/nav/helse/sparsom/db/HendelseRepository.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.helse.sparsom.db
-
-import java.time.LocalDateTime
-import java.util.*
-
-internal interface HendelseRepository {
-    fun lagre(fødselsnummer: String, hendelseId: UUID, json: String, tidsstempel: LocalDateTime): Long
-}
