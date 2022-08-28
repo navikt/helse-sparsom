d157b1befa89c37e9d69393fe118ecff0e0590ba
David Steinsland <david@steinsland.email>
2022-08-28 18:17:23 +0200

    hopper over unike typer

--- src/main/resources/db/migration/V5__normaliser_steg1.sql
+++ src/main/resources/db/migration/V5__normaliser_steg1.sql
@@ -1,3 +1,4 @@
 insert into kontekst_type(type)
 select kontekst_type from aktivitet_kontekst_denormalisert
+on conflict do nothing
 ;
--- src/main/resources/db/migration/V6__normaliser_steg2.sql
+++ src/main/resources/db/migration/V6__normaliser_steg2.sql
@@ -1,5 +1,4 @@
 insert into kontekst_navn(navn)
 select kontekst_navn from aktivitet_kontekst_denormalisert
 on conflict do nothing;
-
 ;
