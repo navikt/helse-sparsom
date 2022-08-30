f432bd51a9afdd62d972966b474187bec667b6f4
David Steinsland <david@steinsland.email>
2022-08-30 14:30:39 +0200

    oppdaterer spørring til å hente ut unike meldinger først
    tuner postgres litt mer

--- src/main/resources/db/migration/V5__normaliser_steg1.sql
+++ src/main/resources/db/migration/V5__normaliser_steg1.sql
@@ -1,3 +1,5 @@
+create index if not exists aktivitet_denorm_melding on aktivitet_denormalisert(melding);
 insert into melding(tekst)
-select melding from aktivitet_denormalisert
-on conflict (tekst) do nothing;
+select distinct melding from aktivitet_denormalisert order by melding
+on conflict do nothing
+;
