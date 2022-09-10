0d477a6a84d489c0c7059d9421696058c36d9f2e
David Steinsland <david@steinsland.email>
2022-09-10 11:14:25 +0200

    tester om to ekstra indekser hjelper til

---
+++ src/main/resources/db/migration/V20__aktivitet_id_indeks.sql
@@ -0,0 +1,2 @@
+create index if not exists idx_aktivitet_kontekst_aktivitet_id on aktivitet_kontekst(aktivitet_id);
+create index if not exists idx_aktivitet_kontekst_navn_id on aktivitet_kontekst(kontekst_navn_id);
