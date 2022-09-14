28005f9383193385207b8d794ebb83f8079222a5
David Steinsland <david@steinsland.email>
2022-09-14 18:31:35 +0200

    forbered import av json

--- src/main/kotlin/no/nav/helse/sparsom/App.kt
+++ src/main/kotlin/no/nav/helse/sparsom/App.kt
@@ -26,7 +26,7 @@ private fun createApp(env: Map<String, String>): RapidsConnection {
 
                 val connection = dataSource.connection
 
-                /*val spleisConfig = HikariConfig().apply {
+                val spleisConfig = HikariConfig().apply {
                     jdbcUrl = String.format(
                         "jdbc:postgresql:///%s?%s&%s",
                         env["DATABASE_SPARSOM_DATABASE"],
@@ -41,10 +41,12 @@ private fun createApp(env: Map<String, String>): RapidsConnection {
                 }
                 val spleisDs = HikariDataSource(spleisConfig)
                 HentAktivitetslogg(Dispatcher("arbeidstabell_step1", connection))
-                    .migrate(connection, spleisDs.connection)*/
+                    .migrate(connection, spleisDs.connection)
 
                 ImporterAktivitetslogg(Dispatcher("arbeidstabell_step2", connection))
                     .migrate(connection)
+
+                exitProcess(0)
             }
 
             override fun onShutdown(rapidsConnection: RapidsConnection) {
