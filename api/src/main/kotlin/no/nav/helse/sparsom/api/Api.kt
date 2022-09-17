0dd9cb1954f5a260238b911b9c19a85074be93b2
David Steinsland <david@steinsland.email>
2022-09-17 11:30:39 +0200

    hente aktiviteter fra kontekst eller ident

--- api/src/main/kotlin/no/nav/helse/sparsom/api/Api.kt
+++ api/src/main/kotlin/no/nav/helse/sparsom/api/Api.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.sparsom.api
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
@@ -19,9 +20,37 @@ internal fun Application.api(dataSource: DataSource, authProviderName: String) {
                 ))
             }
         }
+        get("/api/aktiviteter") {
+            withContext(Dispatchers.IO) {
+                val kontekstNavn = call.queryParam("navn").firstNotNullOfOrNull(::alphaNumericalOnlyOrNull)
+                val kontekstVerdi = call.queryParam("verdi").firstNotNullOfOrNull(::alphaNumericalOnlyOrNull)
+                val ident = call.queryParam("ident").firstNotNullOfOrNull(::numericalOnlyOrNull)
+
+                val aktiviteter = when {
+                    kontekstNavn != null && kontekstVerdi != null -> dao.hentAktiviteterFor(kontekstNavn, kontekstVerdi)
+                    ident != null -> dao.hentAktiviteterFor(ident)
+                    else -> emptyList()
+                }
+
+                call.respond(mapOf("aktiviteter" to aktiviteter))
+            }
+        }
 
         authenticate(authProviderName) {
 
         }
     }
 }
+
+private val re = Regex("[^A-Za-z0-9æøåÆØÅ_-]")
+private fun alphaNumericalOnlyOrNull(str: String): String? {
+    return re.replace(str, "").takeIf(String::isNotEmpty)
+}
+
+private val reNumerical = Regex("[^0-9]")
+private fun numericalOnlyOrNull(str: String): String? {
+    return reNumerical.replace(str, "").takeIf(String::isNotEmpty)
+}
+
+private fun ApplicationCall.queryParam(name: String): List<String> =
+    request.queryParameters.getAll(name)?.filter(String::isNotBlank) ?: emptyList()
--- api/src/main/kotlin/no/nav/helse/sparsom/api/dao/AktivitetDao.kt
+++ api/src/main/kotlin/no/nav/helse/sparsom/api/dao/AktivitetDao.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.sparsom.api.dao
 
+import kotliquery.Row
 import kotliquery.queryOf
 import kotliquery.sessionOf
 import org.intellij.lang.annotations.Language
@@ -15,7 +16,36 @@ internal class AktivitetDao(private val dataSource: DataSource) {
         }.asList)
     }
 
+    fun hentAktiviteterFor(kontekstNavn: String, kontekstVerdi: String) = sessionOf(dataSource).use { session ->
+        session.run(queryOf(AKTIVITETER_FOR_IDENT, kontekstVerdi, kontekstNavn).map(::mapRow).asList)
+    }
+
+    fun hentAktiviteterFor(ident: String) = sessionOf(dataSource).use { session ->
+        session.run(queryOf(AKTIVITETER_FOR_IDENT, ident).map(::mapRow).asList)
+    }
+
+    private fun mapRow(row: Row) =
+        mapOf(
+            "tidsstempel" to row.string("tidsstempel"),
+            "tekst" to row.string("tekst"),
+            "kontekster" to row.string("kontekster")
+                .split(ROW_SEPARATOR)
+                .map {
+                    val verdier = it.split(VALUE_SEPARATOR)
+                    Triple(verdier[0], verdier[1], verdier[2])
+                }
+                .groupBy(Triple<String, *, *>::first)
+                .mapValues { verdier ->
+                    verdier.value.associate { kontekstverdi ->
+                        kontekstverdi.second to kontekstverdi.first
+                    }
+                }
+        )
+
     private companion object {
+        private const val VALUE_SEPARATOR = ','
+        private const val ROW_SEPARATOR = ';'
+
         @Language("PostgreSQL")
         private val VARSLER = """ 
              with
@@ -29,6 +59,43 @@ internal class AktivitetDao(private val dataSource: DataSource) {
             join melding on id = melding_id
             order by antall desc
 ;
+        """
+
+        @Language("PostgreSQL")
+        private val AKTIVITETER_FOR_KONTEKST = """
+            with aktiviteter as materialized (
+                select ak.aktivitet_id
+                from aktivitet_kontekst ak
+                inner join kontekst_verdi kv on ak.kontekst_verdi_id = kv.id
+                inner join kontekst_navn k on ak.kontekst_navn_id = k.id
+                where kv.verdi = ? and k.navn=?
+            )
+            select a.id, a.level, a.tidsstempel, m.tekst, string_agg(concat_ws('$VALUE_SEPARATOR', kt.type, kn.navn, kv.verdi), '$ROW_SEPARATOR') as kontekster 
+            from aktivitet a
+            inner join personident p on p.id = a.personident_id
+            inner join melding m on a.melding_id = m.id
+            inner join aktivitet_kontekst ak on a.id = ak.aktivitet_id
+            inner join kontekst_type kt on ak.kontekst_type_id = kt.id
+            inner join kontekst_navn kn on kn.id = ak.kontekst_navn_id
+            inner join kontekst_verdi kv on kv.id = ak.kontekst_verdi_id
+            where a.id in (SELECT aktivitet_id FROM aktiviteter)
+            group by a.id, a.tidsstempel, m.tekst
+            order by tidsstempel;
+        """
+
+        @Language("PostgreSQL")
+        private val AKTIVITETER_FOR_IDENT = """ 
+            select a.id, a.level, a.tidsstempel, m.tekst, string_agg(concat_ws('$VALUE_SEPARATOR', kt.type, kn.navn, kv.verdi), '$ROW_SEPARATOR') as kontekster 
+            from aktivitet a
+            inner join personident p on p.id = a.personident_id
+            inner join melding m on a.melding_id = m.id
+            inner join aktivitet_kontekst ak on a.id = ak.aktivitet_id
+            inner join kontekst_type kt on ak.kontekst_type_id = kt.id
+            inner join kontekst_navn kn on kn.id = ak.kontekst_navn_id
+            inner join kontekst_verdi kv on kv.id = ak.kontekst_verdi_id
+            where p.ident=?
+            group by a.id, a.tidsstempel, m.tekst
+            order by tidsstempel;
         """
     }
 }
