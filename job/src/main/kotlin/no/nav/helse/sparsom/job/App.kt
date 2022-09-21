50b4025f12b3c6cc5a1a0802fe525733fb2b4dbc
David Steinsland <david@steinsland.email>
2022-09-21 15:47:49 +0200

    må koble til via cloudsql socket factory

--- job/src/main/kotlin/no/nav/helse/sparsom/job/App.kt
+++ job/src/main/kotlin/no/nav/helse/sparsom/job/App.kt
@@ -17,16 +17,22 @@ private val sikkerLogg = LoggerFactory.getLogger("tjenestekall")
 
 fun main() {
     val env = System.getenv()
-    val databaseHost: String = requireNotNull(env["DATABASE_HOST"]) { "host må settes" }
-    val databasePort: String = requireNotNull(env["DATABASE_PORT"]) { "port må settes" }
+    val databaseInstance = requireNotNull(env["DATABASE_INSTANCE"]) { "database instance must be set" }
+    val databaseRegion: String = requireNotNull(env["DATABASE_REGION"]) { "DATABASE_REGION må settes" }
+    val gcpProjectId: String = requireNotNull(env["GCP_TEAM_PROJECT_ID"]) { "GCP_TEAM_PROJECT_ID må settes" }
     val databaseName: String = requireNotNull(env["DATABASE_DATABASE"]) { "databasenavn må settes" }
     val dbConfig = HikariConfig().apply {
-        jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s", databaseHost, databasePort, databaseName)
+        jdbcUrl = String.format(
+            "jdbc:postgresql:///%s?%s&%s",
+            databaseName,
+            "cloudSqlInstance=$gcpProjectId:$databaseRegion:$databaseInstance",
+            "socketFactory=com.google.cloud.sql.postgres.SocketFactory"
+        )
         username = requireNotNull(env["DATABASE_USERNAME"]) { "brukernavn må settes" }
         password = requireNotNull(env["DATABASE_PASSWORD"]) { "passord må settes" }
         initializationFailTimeout = Duration.ofMinutes(1).toMillis()
         connectionTimeout = Duration.ofMinutes(1).toMillis()
-        maximumPoolSize = 2
+        maximumPoolSize = 1
     }
 
     HikariDataSource(dbConfig).connection.use {
