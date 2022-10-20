97d6f8053e1fa4e6399d38d6db4d8b80b74a7512
David Steinsland <david@steinsland.email>
2022-10-20 16:51:02 +0200

    forbereder å hente fersk aktivitetslogg en gang til

--- job/src/main/kotlin/no/nav/helse/sparsom/job/App.kt
+++ job/src/main/kotlin/no/nav/helse/sparsom/job/App.kt
@@ -7,6 +7,7 @@ import com.zaxxer.hikari.HikariDataSource
 import org.slf4j.LoggerFactory
 import java.sql.Connection
 import java.time.Duration
+import javax.sql.DataSource
 import kotlin.system.exitProcess
 
 internal val objectMapper = jacksonObjectMapper()
@@ -34,14 +35,39 @@ fun main() {
         connectionTimeout = Duration.ofMinutes(1).toMillis()
         maximumPoolSize = 1
     }
+    val spleisDbConfig = spleisDbConfig(env)
 
-    HikariDataSource(dbConfig).connection.use {
-        runApplication(it)
+    HikariDataSource(dbConfig).connection.use { sparsomConnection ->
+        HikariDataSource(spleisDbConfig).connection.use { spleisConnection ->
+            runApplication(sparsomConnection, spleisConnection)
+        }
     }
 }
 
-private fun runApplication(connection: Connection) {
-    ImporterAktivitetslogg(Dispatcher("arbeidstabell_step2", connection))
-        .migrate(connection)
+private fun spleisDbConfig(env: Map<String, String>): HikariConfig {
+    val databaseInstance = requireNotNull(env["DATABASE_SPARSOM_INSTANCE"]) { "database instance must be set" }
+    val databaseRegion: String = requireNotNull(env["DATABASE_SPARSOM_REGION"]) { "DATABASE_SPARSOM_REGION må settes" }
+    val gcpProjectId: String = requireNotNull(env["GCP_TEAM_PROJECT_ID"]) { "GCP_TEAM_PROJECT_ID må settes" }
+    val databaseName: String = requireNotNull(env["DATABASE_SPARSOM_DATABASE"]) { "databasenavn må settes" }
+    return HikariConfig().apply {
+        jdbcUrl = String.format(
+            "jdbc:postgresql:///%s?%s&%s",
+            databaseName,
+            "cloudSqlInstance=$gcpProjectId:$databaseRegion:$databaseInstance",
+            "socketFactory=com.google.cloud.sql.postgres.SocketFactory"
+        )
+        username = requireNotNull(env["DATABASE_SPARSOM_USERNAME"]) { "brukernavn må settes" }
+        password = requireNotNull(env["DATABASE_SPARSOM_PASSWORD"]) { "passord må settes" }
+        initializationFailTimeout = Duration.ofMinutes(1).toMillis()
+        connectionTimeout = Duration.ofMinutes(1).toMillis()
+        maximumPoolSize = 1
+    }
+}
+
+private fun runApplication(connection: Connection, spleisConnection: Connection) {
+    HentAktivitetslogg(Dispatcher("arbeidstabell_step1", connection))
+        .migrate(connection, spleisConnection)
+    /*ImporterAktivitetslogg(Dispatcher("arbeidstabell_step2", connection))
+        .migrate(connection)*/
 }
 
--- job/src/main/kotlin/no/nav/helse/sparsom/job/HentAktivitetslogg.kt
+++ job/src/main/kotlin/no/nav/helse/sparsom/job/HentAktivitetslogg.kt
@@ -9,7 +9,7 @@ import java.sql.PreparedStatement
 
 internal class HentAktivitetslogg(private val dispatcher: Dispatcher) {
 
-    fun migrate(connection: Connection, spleisConnection: Connection) = spleisConnection.use {
+    fun migrate(connection: Connection, spleisConnection: Connection) {
         connection.autoCommit = false
         spleisConnection.autoCommit = false
         try {
