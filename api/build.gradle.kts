4c4565063be6401de5c1013998050a669d485fcb
david steinsland <david@davidsteinsland.net>
2024-11-17 09:58:32 +0100

    sørger for lik ktorversjon

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,18 +1,16 @@
 val micrometerRegistryPrometheusVersion = "1.12.3"
-val ktorVersion = "3.0.1"
-val awaitilityVersion = "4.1.1"
 val tbdLibsVersion = "2024.11.15-09.09-08ca346b"
 val mainClass = "no.nav.helse.sparsom.api.AppKt"
 
 dependencies {
     implementation("io.micrometer:micrometer-registry-prometheus:$micrometerRegistryPrometheusVersion")
     implementation("com.github.navikt.tbd-libs:azure-token-client-default:$tbdLibsVersion")
-    implementation("io.ktor:ktor-server-netty:$ktorVersion")
-    implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
-    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
-    implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
-    implementation("io.ktor:ktor-server-call-id:$ktorVersion")
-    implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion") {
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-metrics-micrometer")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-jackson")
+    implementation("io.ktor:ktor-server-call-id")
+    implementation("io.ktor:ktor-server-auth-jwt") {
         exclude(group = "junit")
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
+val ktorVersion = "3.0.1"
 val opensearchClientVersion = "2.3.0"
 val jacksonVersion = "2.18.1"
 val junitJupiterVersion = "5.11.3"
@@ -37,7 +38,29 @@ allprojects {
     }
 
     dependencies {
-        implementation("com.jillesvangurp:search-client:$opensearchClientVersion")
+        constraints {
+            api("io.ktor:ktor-server-netty:$ktorVersion")
+            api("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
+            api("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+            api("io.ktor:ktor-serialization-jackson:$ktorVersion")
+            api("io.ktor:ktor-server-call-id:$ktorVersion")
+            api("io.ktor:ktor-server-auth-jwt:$ktorVersion")
+        }
+        implementation("com.jillesvangurp:search-client:$opensearchClientVersion") {
+            /* clamper search-client til å bruke samme ktorversjon som api-modulen */
+            constraints {
+                api("io.ktor:ktor-client-cio:$ktorVersion")
+                api("io.ktor:ktor-client-java:$ktorVersion")
+                api("io.ktor:ktor-client-core:$ktorVersion")
+                api("io.ktor:ktor-client-auth:$ktorVersion")
+                api("io.ktor:ktor-client-logging:$ktorVersion")
+                api("io.ktor:ktor-client-serialization:$ktorVersion")
+                api("io.ktor:ktor-serialization-kotlinx:$ktorVersion")
+                api("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                api("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                api("io.ktor:ktor-client-darwin:$ktorVersion")
+            }
+        }
         implementation("ch.qos.logback:logback-classic:$logbackClassicVersion")
         implementation("net.logstash.logback:logstash-logback-encoder:$logbackEncoderVersion") {
             exclude("com.fasterxml.jackson.core")
