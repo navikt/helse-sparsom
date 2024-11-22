84153ca6c25a93a76cab1f6cabbdcdaef472c96d
david steinsland <david@davidsteinsland.net>
2024-11-22 09:07:47 +0100

    legger til info i mdc entries og ktor-request-metrikken

--- api/src/main/kotlin/no/nav/helse/sparsom/api/App.kt
+++ api/src/main/kotlin/no/nav/helse/sparsom/api/App.kt
@@ -7,6 +7,10 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.navikt.tbd_libs.naisful.naisApp
 import com.jillesvangurp.ktsearch.SearchClient
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.auth.jwt.JWTPrincipal
+import io.ktor.server.auth.principal
+import io.ktor.server.request.header
 import io.micrometer.core.instrument.Clock
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
@@ -34,7 +38,18 @@ internal fun createApp(azureConfig: AzureAdAppConfig, searchClient: SearchClient
         meterRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT, PrometheusRegistry.defaultRegistry, Clock.SYSTEM),
         objectMapper = objectMapper,
         applicationLogger = LoggerFactory.getLogger(::main::class.java),
-        callLogger = LoggerFactory.getLogger("tjenestekall")
+        callLogger = LoggerFactory.getLogger("tjenestekall"),
+        timersConfig = { call, _ ->
+            this
+                .tag("azp_name", call.principal<JWTPrincipal>()?.get("azp_name") ?: "n/a")
+                // https://github.com/linkerd/polixy/blob/main/DESIGN.md#l5d-client-id-client-id
+                // eksempel: <APP>.<NAMESPACE>.serviceaccount.identity.linkerd.cluster.local
+                .tag("konsument", call.request.header("L5d-Client-Id") ?: "n/a")
+        },
+        mdcEntries = mapOf(
+            "azp_name" to { call: ApplicationCall -> call.principal<JWTPrincipal>()?.get("azp_name") },
+            "konsument" to { call: ApplicationCall -> call.request.header("L5d-Client-Id") }
+        ),
     ) {
         azureAdAppAuthentication(azureConfig)
         api(searchClient, API_SERVICE)
